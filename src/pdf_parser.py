60832643096f4575190bc2bc02580bd4f13325fb
JamesYi <james1996thu@163.com>
2019-03-26 11:46:53 +0800

    init project

---
+++ src/pdf_parser.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+# author: JamesYi
+# created on Mar 26th, 2019
+
+import pdf2txt
+
+
+class PDFParser(object):
+    def __init__(self):
+        pass
+
+    def parser(self):
+        pass
+
+
+if __name__ == '__main__':
+    pdf_parser = PDFParser()
