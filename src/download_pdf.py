585bdf1ccb83b387da39715cbae5a6a2c346f379
jamesyi <james1996thu@gmail.com>
2019-03-26 15:27:42 +0800

    modify download

--- src/download_pdf.py
+++ src/download_pdf.py
@@ -2,24 +2,19 @@
 # author: JamesYi
 # created on Mar 26th, 2019
 
-from urllib.request import urlretrieve
 import os
 import json
-from progressbar import *
+import re
 
 def download_pdf():
     paper_list = json.loads(open('../data/arxivData.json', 'r').read())
-    total = len(paper_list)
     save_dir = '../data/pdf/'
     if not os.path.exists(save_dir):
         os.mkdir(save_dir)
-    p_bar = ProgressBar().start()
-    for index, paper in enumerate(paper_list):
-        _id = paper['id']
-        _link = paper['link'][1]['href'].replace('//', '//cn.')
-        urlretrieve(_link, save_dir + _id + '.pdf')
-        p_bar.update(int((index + 1)/ total * 100))
-    p_bar.finish()
+    with open('../data/pdf_links.txt', 'w') as f:
+        for index, paper in enumerate(paper_list):
+            _link = re.findall(r'http://arxiv\.org/pdf/.*v\d', paper['link'])[0].replace('arxiv', 'cn.arxiv')
+            f.write('{}\n'.format(_link))
 
 if __name__ == '__main__':
     download_pdf()
